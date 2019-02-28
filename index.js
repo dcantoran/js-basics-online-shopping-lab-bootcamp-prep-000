d1f59a707d6023b0f07af61b31f0cf1838483aad
David Cantoran <davidcantoran36@gmail.com>
2019-02-28 02:26:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ return `${item} has been added to your cart.`;
 function viewCart() {
   // write your code here
   if(cart.length === 0){
-    console.log("Your shopping cart is empty.")
+    return ("Your shopping cart is empty.")
   }
 }
 
