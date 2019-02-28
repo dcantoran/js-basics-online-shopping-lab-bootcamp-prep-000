642c49941cd9562b5ff6a465f262428c5ac94a45
David Cantoran <davidcantoran36@gmail.com>
2019-02-28 02:59:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -39,6 +39,10 @@ function viewCart() {
 
 function total() {
   // write your code here
+  var result = 0;
+  for (var i = 0; i < cart.length; i ++) {
+    
+  }
 }
 
 function removeFromCart(item) {
