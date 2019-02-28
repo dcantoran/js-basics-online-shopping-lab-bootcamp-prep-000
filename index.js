8e6797da7b671e2c9d05fdf680e760f2281eeec1
David Cantoran <davidcantoran36@gmail.com>
2019-02-28 03:04:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -62,5 +62,10 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   // write your code here
-  
+  if (undefined){
+    return "Sorry, we don't have a credit card on file for you."
+  } else {
+    cart = [];
+    return `Your total cost is $${total}, which will be charged to the card ${cardNumber}.`
+  }
 }
