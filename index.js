57fed617cf45d58aa8097c7655c759b4b6d047f2
David Cantoran <davidcantoran36@gmail.com>
2019-02-28 03:12:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -67,5 +67,6 @@ function placeOrder(cardNumber) {
   } else {
     cart = [];
     
-  }return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`;
+  }
+  return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`;
 }
