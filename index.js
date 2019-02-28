b10ae2c5b07d87b2677c61e38c322487e11e1942
David Cantoran <davidcantoran36@gmail.com>
2019-02-28 02:53:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,15 +23,17 @@ function viewCart() {
   // write your code here
   
   if (cart.length === 0) {
-    return ("Your shopping cart is empty.")
+    return "Your shopping cart is empty."
+  } else if (cart.length === 1) {
+    return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}.`
+  } else if (cart.length === 2) {
+    return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}, and ${cart[1].itemName} at $${cart[1].itemPrice}.`
   } else {
-    var items = [];
-    for (var i = 0; i < cart.length; i++) {
-      for (var item in cart[i]) {
-        items.push(item + " at $" + cart[i][item])
-      }
+    var myString = "In your cart, you have "
+    for(var i = 0; i < cart.length - 1; i++) {
+      myString = myString + `${cart[i].itemName} at $${cart[i].itemPrice}, `
     }
-    return ("In your cart, you have " + items.join(", ") + ".");
+    return myString + `and ${cart[cart.length - 1].itemName} at $${cart[cart.length - 1].itemPrice}.`
   }
 }
 
