132471da0028c220f7010bc4aa4f46a0a28df38f
David Cantoran <davidcantoran36@gmail.com>
2019-02-28 02:02:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,6 +25,7 @@ function viewCart() {
     if(cart.length === 0) {
       console.log("Your shopping cart is empty.");
     }
+    var arr = [];
     for(var i = 0; i < cart.length; i ++) {
       if(cart.length === 1) {
         console.log(`In your cart, you have ${cart[0]} at`)
